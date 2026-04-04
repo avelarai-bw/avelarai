eefb144ac54c38a78b67753ded041e073eba4305
nores7 <johnianbanda@gmail.com>
2026-04-04 16:17:22 +0200

    fix: cors for production and cleanup warnings

--- Frontend/src/pages/Auth/Auth.tsx
+++ Frontend/src/pages/Auth/Auth.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import Navbar from '../../components/Navbar/Navbar';
-//import { useAuth } from '../../hooks/useAuth';
+
 import api from '../../services/api';
 import type { AuthResponse } from '../../types';
 import styles from './Auth.module.css';
